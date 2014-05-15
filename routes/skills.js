3630e35871766c3f7147a70d5e8b834692dceab9
Charles Bushong <bushong1@gmail.com>
2014-05-15 19:41:17 +0000

    Starting work on the algorithm

--- routes/skills.js
+++ routes/skills.js
@@ -30,24 +30,73 @@ router.get(/^\/(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)
 
   var sorted_skills = [];
 
+  // Calculate present cost for each skill
   for ( var skill_name in skills){
-    if(skills[skill_name]['levels'].length != skills[skill_name]['current_level']){
+/*  if(skills[skill_name]['levels'].length != skills[skill_name]['current_level']){
       skills[skill_name]['cost_this_level'] = parseInt(skills[skill_name]['levels'][skills[skill_name]['current_level']]) + user_level*10;
     } else {
       skills[skill_name]['cost_this_level'] = "MAX";
     }
-
-    sorted_skills.push(skills[skill_name]);
-    console.log(JSON.stringify(skills[skill_name]));
+*/
+    var skill = JSON.parse(JSON.stringify((skills[skill_name])));
+    skill['name'] = skill_name;
+    sorted_skills.push(skill);
+    console.log(JSON.stringify(skill));
   }
 
-  sorted_skills.sort( function(a, b){
-    return a['cost_this_level'] - b['cost_this_level'];
-  });
+  // Sort skills by cost, increasing
+  sorted_skills.sort( sort_skills );
 
-  //res.send("Skills with levels: "+JSON.stringify(skills));
-  res.render('skills', {"skills" :  skills, 'user_level':user_level, 'gold':gold});
+  res.send("Skills with levels: "+JSON.stringify(skills));
+  //res.render('skills', {"skills" :  skills, 'user_level':user_level, 'gold':gold});
 });
+
+function sort_skills(a, b){
+  return a.levels[a.current_level] - b.levels[b.current_level];
+}
+
+function get_set_of_available_purchases(skills_array, gold){
+
+  // Return if skills_array is empty  
+  if(skills_array == null || skills_array.length == 0){
+    return [];
+  }
+
+  // Return nothing when the cheapest skill in the array is too expensive
+  if(skills_array[0].cost_this_level > gold){
+    return [];
+  }
+//Algorithm
+//States:
+//Empty List || Cheapest Too expensive
+  //Return empty set
+//Skill->
+  //Not Used 
+    //call subs, full money
+      //Add to set
+  //Used no upgrades
+    //call subs, money - skill cost - 10
+    //Push skill to all
+      //Add to set
+  //Used add upgrades
+    //Check if upgrade needed
+    //call subs + upgrade, money - skill cost - 10
+      //Push skill to all
+      //Add to set + Skill
+  //Return set
+  var skills = skills_array.slice(0);
+
+  current_skill = skills.shift();
+  get_set_of_available_purchases(sub_set, gold - parseInt(current_skill.cost_this_level) - 10);
+}
+
+function add_level(skill){
+  //todo  
+}
+
+function cost_this_level(skill, level){
+  return skill.levels[skill.current_level] + level * 10;
+}
 
 router.get("/", function(req, res) {
   res.redirect('/skills/0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,100');
