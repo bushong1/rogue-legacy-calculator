be525799fe39d54e1c9c6209c43cb529cec75886
Charles Bushong <bushong1@gmail.com>
2014-05-20 02:34:08 +0000

    adding favicon, cleaning up some express stuff.

--- app.js
+++ app.js
@@ -1,12 +1,10 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('static-favicon');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
-var routes = require('./routes/index');
-var users = require('./routes/users');
 var skills = require('./routes/skills');
 
 var app = express();
@@ -15,15 +13,13 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
-app.use(favicon());
+app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', routes);
-app.use('/users', users);
 app.use('/skills', skills);
 
 /// catch 404 and forwarding to error handler
